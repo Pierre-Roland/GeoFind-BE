f833f6a5008e3ea0f1a5bcf2f9c019a9c3bac3cc
Pierre Roland <127956402+pierreroland04@users.noreply.github.com>
2025-09-18 14:35:24 +0200

    feat: initial config

---
+++ main/java/roll/be/geofind/GeoFindApplication.java
@@ -0,0 +1,13 @@
+package roll.be.geofind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoFindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoFindApplication.class, args);
+    }
+
+}
---
+++ test/java/roll/be/geofind/GeoFindApplicationTests.java
@@ -0,0 +1,13 @@
+package roll.be.geofind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoFindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
