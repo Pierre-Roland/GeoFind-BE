a6891c2430a506bc2762845c817d52de8aa04fab
Pierre Roland <127956402+pierreroland04@users.noreply.github.com>
2025-09-30 22:24:41 +0200

    feat: ajout france in db

--- src/main/resources/db/migration/V4__insert_data_coordonnees.sql
+++ src/main/resources/db/migration/V4__insert_data_coordonnees.sql
@@ -1,3 +1,4 @@
 -- Insertion de données initiales
 INSERT INTO geofind.coordonnees (zoom, center1, center2, country) VALUES
-(7.5, 50.505, 5, 'Belgique');
+(7.5, 50.8503, 4.3517, 'Belgique'),
+(6, 46.603354, 1.888334, 'France');
