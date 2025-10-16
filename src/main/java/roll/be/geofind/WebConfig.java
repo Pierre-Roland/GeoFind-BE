41600659f1bffd6aceaf3209570f3792345636d9
Pierre Roland <127956402+pierreroland04@users.noreply.github.com>
2025-10-16 23:22:54 +0200

    fix: changement du docker-compose.2

--- src/main/java/roll/be/geofind/WebConfig.java
+++ src/main/java/roll/be/geofind/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://ton-front-url.render.com")
+                        .allowedOrigins("http://localhost:4200", "https://geofindroll.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
             }
