7b7c2e205630c35b2d1ba7be793a942049379b54
Pierre Roland <127956402+pierreroland04@users.noreply.github.com>
2025-10-16 19:37:37 +0200

    fix: changement du docker-compose.2

--- src/main/resources/db/migration/V6__create_table_password_reset_token.sql
+++ src/main/resources/db/migration/V6__create_table_password_reset_token.sql
@@ -1,12 +1,9 @@
-CREATE TABLE IF NOT EXISTS test.password_reset_token (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS geofind.password_reset_token (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     token VARCHAR(100) NOT NULL,
     expirydate TIMESTAMP NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id BIGINT NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES geofind.users(id) ON DELETE CASCADE,
     CONSTRAINT uq_token UNIQUE (token)
 );
-
--- Ajouter la FK séparément pour éviter certains problèmes TiDB
-ALTER TABLE test.password_reset_token
-    ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES geofind.users(id) ON DELETE CASCADE;
